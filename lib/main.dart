bf1690161ad8cc8141b8db976253866b1a805567
Miguel Licea <66740053+miglcesp01@users.noreply.github.com>
2022-08-20 18:42:37 -0500

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,40 +36,212 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   XFile? imagenSeleccionada;
   final ImagePicker _picker = ImagePicker();
+  bool _laoding = true;
+  late File _image;
+  late List _output;
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Tflite.close();
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 36,
+        threshold: 0.5,
+        imageMean: 127.5,
+        imageStd: 127.5);
+    setState(() {
+      _output = output!;
+      _laoding = false;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: 'assets/model.tflite', labels: 'assets/labels.txt');
+  }
+
+  pickImage() async {
+    var image = await _picker.pickImage(source: ImageSource.camera);
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+    classifyImage(_image);
+  }
+
+  pickGalleryImage() async {
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+    classifyImage(_image);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.black,
+        title: Text(
+          'Skin Cancer Classification',
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w200,
+              fontSize: 20,
+              letterSpacing: 0.8),
+        ),
       ),
-      body: Column(
-        children: <Widget>[
-          const Text(
-            'Selecciona una foto y espera que el modelo genere el resultado',
+      body: Container(
+        color: Colors.black.withOpacity(0.9),
+        padding: EdgeInsets.symmetric(horizontal: 35, vertical: 50),
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            color: Color(0xFF2A363B),
+            borderRadius: BorderRadius.circular(30),
           ),
-          TextButton(
-            onPressed: () async {
-              XFile? imagen =
-                  await _picker.pickImage(source: ImageSource.gallery);
-              setState(() {
-                imagenSeleccionada = imagen;
-              });
-              if (imagenSeleccionada != null) {
-                _showImage(imagenSeleccionada!);
-              } else {}
-            },
-            child: const Text("Seleccionar una foto"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Center(
+                  child: _laoding == true
+                      ? null //show nothing if no picture selected
+                      : Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 250,
+                                width: 250,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(30),
+                                  child: Image.file(
+                                    _image,
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                              Divider(
+                                height: 25,
+                                thickness: 1,
+                              ),
+                              _output != null
+                                  ? Text(
+                                      'The object is: ${_output[0]['label']}!',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w400),
+                                    )
+                                  : Container(),
+                              Divider(
+                                height: 25,
+                                thickness: 1,
+                              ),
+                            ],
+                          ),
+                        ),
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: pickImage, //no parenthesis
+                      child: Container(
+                        width: MediaQuery.of(context).size.width - 200,
+                        alignment: Alignment.center,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+                        decoration: BoxDecoration(
+                            color: Colors.blueGrey[600],
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Text(
+                          'Take A Photo',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    GestureDetector(
+                      onTap: pickGalleryImage, //no parenthesis
+                      child: Container(
+                        width: MediaQuery.of(context).size.width - 200,
+                        alignment: Alignment.center,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+                        decoration: BoxDecoration(
+                            color: Colors.blueGrey[600],
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Text(
+                          'Pick From Gallery',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          SizedBox(
-            child: imagenSeleccionada != null
-                ? Image.file(File(imagenSeleccionada!.path))
-                : const Text("No ha seleccionado ninguna imagen"),
-          ),
-        ],
+        ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-
-  _showImage(XFile image) {}
 }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text(widget.title),
+  //     ),
+  //     body: Column(
+  //       children: <Widget>[
+  //         const Text(
+  //           'Selecciona una foto y espera que el modelo genere el resultado',
+  //         ),
+  //         TextButton(
+  //           onPressed: () async {
+  //             XFile? imagen =
+  //                 await _picker.pickImage(source: ImageSource.gallery);
+  //             setState(() {
+  //               imagenSeleccionada = imagen;
+  //             });
+  //             if (imagenSeleccionada != null) {
+  //               _showImage(imagenSeleccionada!);
+  //             } else {}
+  //           },
+  //           child: const Text("Seleccionar una foto"),
+  //         ),
+  //         SizedBox(
+  //           child: imagenSeleccionada != null
+  //               ? Image.file(File(imagenSeleccionada!.path))
+  //               : const Text("No ha seleccionado ninguna imagen"),
+  //         ),
+  //       ],
+  //     ),
+  //     // This trailing comma makes auto-formatting nicer for build methods.
+  //   );
+  // }
+
+  // _showImage(XFile image) {}
+
